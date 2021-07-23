4bc34fa7521272a270d4600108e0a9e930506030
Yurley Solimer <yurley.solimer@gmail.com>
2021-07-23 16:25:52 -0400

    verify token

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 const router = Router()
 
-import * as productsCtrl from "../controllers/products.controller";
+import * as productsCtrl from "../controllers/products.controller"
+import { verifyToken } from '../middlewares'
 
 router.get('/', productsCtrl.getProducts)
-router.post('/', productsCtrl.createProduct)
+router.post('/', verifyToken, productsCtrl.createProduct)
 router.get('/:id', productsCtrl.getProduct)
-router.delete('/:id', productsCtrl.deleteProduct)
-router.put('/:id', productsCtrl.updateProduct)
+router.delete('/:id', verifyToken, productsCtrl.deleteProduct)
+router.put('/:id', verifyToken, productsCtrl.updateProduct)
 
 
 export default router;
